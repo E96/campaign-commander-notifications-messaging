fcdd128ff16c53e5f014f9ba644fe39980fecbd9
Vasily Ostanin <bazilio91@gmail.com>
2015-07-21 15:19:54 +0500

    update package

---
+++ CampaignCommanderNotificationMessagingException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace e96\campaigcommandernnotificationmessaging;
+
+/**
+ * Campaign Commander Notification Messaging
+ *
+ * @author    Tijs Verkoyen <php-campaign-commander-notificiation-messaging@verkoyen.eu>
+ */
+class CampaignCommanderNotificationMessagingException extends \Exception
+{
+}
